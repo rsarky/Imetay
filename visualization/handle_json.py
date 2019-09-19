01fb7756642060bb023d3b3c3a5ff7fb98b160f9
yashYRS <ysarrof@acm.org>
2019-09-19 16:55:57 +0530

    Visualization : first basic visualization done

--- visualization/handle_json.py
+++ visualization/handle_json.py
@@ -7,7 +7,6 @@ def getDataFromFile ( filename ) :
 		data = json.load ( dfile )
 	return data 
 
-
 def analyzeDict ( someDict ) : 
 	## get the params in each of doctor / patient / appointment 
 	for k1 in someDict : 
@@ -33,7 +32,46 @@ def correctTime ( inputDict ) :
 	return inputDict
 		
 
-def startVisualization( filename ) : 
+def get_departments ( doctors ) : 
+
+	dept_list = [] 
+	for k in doctors : 
+		dept_list.append( doctors[k]["department"] ) 
+	return list ( set ( dept_list ) ) 
+
+def get_doctor_list ( doctors , dept_name ) : 
+
+	doc_list = [] 
+	for k in doctors : 
+		if doctors[k]["department"] == dept_name : 
+			doc_list.append ( k ) 
+	return list(set( doc_list ) ) 
+
+def get_ailments (appointments, doc_list ) : 
+
+	ailment_list = []
+
+	for k in appointments : 
+		if appointments[k]["doctorUID"] in doc_list : 
+			ailment_list.append ( appointments[k]["ailment"]) 
+
+	return list ( set ( ailment_list ) ) 
+
+def get_dept_info ( doctors , appointments ) : 
+
+	ailment_dept = {} 
+	doc_dept = {}
+	## get list of departments 
+	dept_list = get_departments(doctors)
+	## ailment per department
+	for dept_name in dept_list : 
+		
+		doc_dept[dept_name] = get_doctor_list ( doctors , dept_name )
+		ailment_dept [ dept_name ] = get_ailments ( appointments , doc_dept[dept_name] )
+
+	return ailment_dept , doc_dept
+
+def preprocessData( filename ) : 
 	
 	## entry point for the script to run 
 	data = getDataFromFile ( filename )
@@ -46,11 +84,4 @@ def startVisualization( filename ) :
 	appointments = correctWaitingTime ( appointments )
 	appointments = correctTime ( appointments )
 	
-	analyzeDict(appointments)
-
-
-## for appointments, doctors -> everything is in string | patient only name is via string | phone number and noShows ## 
-## doctor ->  dept |  First Name ; patients -> noShows | name ; appointments -> ailment | appointment | docID | patID | wait time 
-
-## function to start the python script 
-startVisualization('data.json')
+	return  patients , appointments , doctors 
